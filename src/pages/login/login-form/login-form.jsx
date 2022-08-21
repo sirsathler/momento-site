0f2e65f4216819be207a85b06b741d46019172dc
Matheus Sathler <mattsathler@gmail.com>
2022-08-21 19:24:24 -0300

    Starting feed page

--- src/pages/login/login-form/login-form.jsx
+++ src/pages/login/login-form/login-form.jsx
@@ -5,30 +5,31 @@ import { TextField, Button } from '@mui/material'
 export default function Login() {
     return (
         <div className="login-form">
-            <form className="l-form-container">
-                <h1 className="l-form-container__title forte-font">Bem vindo!</h1>
+            <div className="l-form-wrapper">
+                <form className="l-form-inner">
+                    <h1 className="l-form-inner__title forte-font">Bem vindo!</h1>
+                    <TextField
+                        className="l-form-inner__text"
+                        label="Email"
+                        variant="standard"
+                        required={true}
+                    />
 
-                <TextField
-                    className="l-form-container__text"
-                    label="Email"
-                    variant="standard"
-                    required={true}
-                />
+                    <TextField sx={{ mt: '26px' }}
+                        className="l-form-inner__text"
+                        label="Senha" variant="standard"
+                        type="password"
+                        required={true}
+                    />
 
-                <TextField sx={{ mt: '26px' }}
-                    className="l-form-container__text"
-                    label="Senha" variant="standard"
-                    type="password"
-                    required={true}
-                />
-
-                <Button sx={{ mt: '32px' }}
-                    className="l-form-container__button"
-                    variant="contained">
-                    Login
-                </Button>
-            </form>
-            {/* <h2 className='l-form-container__text '>Não possui conta? <span className='primary-color'>Registre-se!</span></h2> */}
+                    <Button sx={{ mt: '32px' }}
+                        className="l-form-inner__button"
+                        variant="contained">
+                        Login
+                    </Button>
+                </form>
+                <h2 className='l-form-wrapper__text '>Não possui conta? <span className='primary-color'>Registre-se!</span></h2>
+            </div>
         </div>
     )
 }
